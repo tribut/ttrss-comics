1e191adda08437c49ae34a4bc52c782fff134bf7
Felix Eckhofer <felix@eckhofer.com>
2013-04-21 17:50:00 +0200

    add af_gucomics

---
+++ af_gucomics/init.php
@@ -0,0 +1,45 @@
+<?php
+class af_gucomics extends Plugin {
+
+	private $host;
+
+	function about() {
+		return array(0.1,
+			'Add images to GU Comics feed',
+			'dxbi');
+	}
+
+	function init($host) {
+		$this->host = $host;
+		$host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
+	}
+
+	function hook_article_filter($article) {
+		if (strpos($article['link'], 'gucomics.com/comic') !== FALSE) {
+			if (preg_match(
+				'/(?P<year>[0-9]{4})(?P<month>[0-9]{2})(?P<day>[0-9]{2})$/',
+				$article['link'],
+				$match
+			)) {
+				// move short descriptions to the title
+				if (preg_match('/[^<>]{0,50}/', $article['content'])) {
+					$article['title'] = $article['content'];
+					$article['content'] = '';
+				}
+				// add image inline
+				$article['content'] = '<p><img src="http://www.gucomics.com/comics/' .
+					$match['year'] . '/gu_' .
+					$match['year'] . $match['month'] . $match['day'] .
+					'.jpg"></p>' . $article['content'];
+			}
+		}
+
+		return $article;
+	}
+
+	function api_version() {
+		return 2;
+	}
+}
+?>
+
